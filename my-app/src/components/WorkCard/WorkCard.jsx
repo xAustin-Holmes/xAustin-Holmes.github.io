41ce199309f02cfe3e08f5149332925118f97f1e
Austin Holmes <AustinDaneHolmes@yahoo.com>
2025-08-20 14:30:21 -0700

    Adjusted WorkCard

--- my-app/src/components/WorkCard/WorkCard.jsx
+++ my-app/src/components/WorkCard/WorkCard.jsx
@@ -1,14 +1,30 @@
+import { useState, useRef } from "react";
 import "./WorkCard.css";
 
 export default function WorkCard({ title, company, fromTo, children }) {
+  const [expanded, setExpanded] = useState(false);
+  const contentRef = useRef(null);
+
+  const toggleExpand = () => setExpanded(!expanded);
+
   return (
-    <div className="work-card">
-        <h2 className="work-card-title">{title}</h2>
-        <div className="work-card-header">
-            <h3 className="work-card-title">{company}</h3>
-            <span className="work-card-dates">{fromTo}</span>
-        </div>
+    <div className="work-card" onClick={toggleExpand} style={{ cursor: "pointer" }}>
+      <h2 className="work-card-title">
+        {title}
+      </h2>
+      <div className="work-card-header">
+        <h3 className="work-card-title">{company}</h3>
+        <span className="work-card-dates">{fromTo}</span>
+      </div>
+
+      
+      <div
+        ref={contentRef}
+        className={`work-card-description-wrapper ${expanded ? "expanded" : ""}`}
+        style={{ maxHeight: expanded ? `${contentRef.current?.scrollHeight}px` : "0px" }}
+      >
         <p className="work-card-description">{children}</p>
+      </div>
     </div>
   );
 }
